c3d073613d8d219adc4bc015d6768d104a605683
brian lin <brianlin224@gmail.com>
2015-10-22 19:00:43 +0800

    case going

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 var $menu    = $('.menu'),
-	$btnMenu = $('.btn-menu');
+	$btnMenu = $('.btn-menu'),
+	$dotnav  = $('.dotnav .list');
 
 $(function(){
 	$btnMenu.on('click', function(){
 		$menu.toggleClass('open');
+	});
+
+	$dotnav.on('click', function(){
+		$(this).addClass('is-curr').siblings().removeClass('is-curr');
 	});
 });
